6bc674969e1fdbca35053512e3134371d5387059
Lara Sanders <larasanders@gmail.com>
2018-01-07 00:12:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
 
-  if  params[:username]) =="" || params[:password]) ==""
+  if  params[:username] =="" || params[:password] ==""
        redirect "/failure"
      else
        user = User.new(:username => params[:username], :password => params[:password])
